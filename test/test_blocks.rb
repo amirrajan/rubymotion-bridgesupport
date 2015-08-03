dcf550914c76dc9fe5a662deb1766bebfac5e62a
Watson <watson1978@gmail.com>
2015-08-03 21:46:26 +0900

    [test] add tests for NS_BLOCKS_AVAILABLE and __BLOCKS__

--- test/test_blocks.rb
+++ test/test_blocks.rb
@@ -7,8 +7,22 @@ class TestBlocks < MiniTest::Unit::TestCase
     @bs = gen_bridge_metadata("blocks.h")
   end
 
+  def test_NS_BLOCKS_AVAILABLE
+    klass = @bs["class"][0]
+    assert_equal(klass["name"], "Bar")
+
+    method = klass["method"]
+    assert_equal(method[0]["selector"], "enumerateObjectsUsingBlock:")
+
+    arg = method[0]["arg"]
+    assert_equal(arg[0]["function_pointer"], "true")
+
+    blocks_arg = arg[0]["arg"]
+    assert_equal(blocks_arg[0]["type"], "@")
+  end
+
   def test_blocks
-    klass = @bs["class"].first
+    klass = @bs["class"][1]
     assert_equal(klass["name"], "Foo")
 
     method = klass["method"]
