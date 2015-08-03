be97e4ef0f9b7a63ed89fa7710cb4855ec672115
Watson <watson1978@gmail.com>
2015-08-03 16:30:31 +0900

    [test] add tests for string constant using CFSTR()

--- test/header/constant.h
+++ test/header/constant.h
@@ -1,2 +1,5 @@
+#import <Foundation/Foundation.h>
+
 #define TestStringConstant "foo"
 #define TestNSStringConstant @"bar"
+#define TestCFStringConstant CFSTR("baz")
--- test/test_constant.rb
+++ test/test_constant.rb
@@ -10,10 +10,12 @@ class TestConstant < MiniTest::Unit::TestCase
   def test_stringconstant
     const = @bs["string_constant"]
 
-    assert_equal(const[0]["name"],  "TestNSStringConstant")
-    assert_equal(const[0]["value"], "bar")
-    assert_equal(const[1]["name"],  "TestStringConstant")
-    assert_equal(const[1]["value"], "foo")
+    assert_equal(const[0]["name"],  "TestCFStringConstant")
+    assert_equal(const[0]["value"], "baz")
+    assert_equal(const[1]["name"],  "TestNSStringConstant")
+    assert_equal(const[1]["value"], "bar")
+    assert_equal(const[2]["name"],  "TestStringConstant")
+    assert_equal(const[2]["value"], "foo")
   end
 
 end
