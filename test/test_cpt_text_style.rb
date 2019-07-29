4d705d04883b2c18ff8ae81fe0322e563c0ee11c
Amir Rajan <ar@amirrajan.net>
2019-07-29 16:37:36 -0500

    Failing test in place for typdef which contains a template macro.

--- test/test_cpt_text_style.rb
+++ test/test_cpt_text_style.rb
@@ -7,8 +7,7 @@ class TestCPTTextStyle < MiniTest::Unit::TestCase
     @bs = gen_bridge_metadata("cpt_text_style.h", {:cflags => "-isysroot '/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator.sdk' -miphoneos-version-min=8.4 -D__ENVIRONMENT_IPHONE_OS_VERSION_MIN_REQUIRED__=80400"})
   end
 
-  def test_category
-
+  def test_typedef_with_template
+    assert_equal(@bs["class"][0]["method"][0]["retval"][0]["type"], "@")
   end
-
 end
